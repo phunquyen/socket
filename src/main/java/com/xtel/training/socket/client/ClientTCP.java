033d3c7ed43a8af17aa29efc4fea19ddf59691ab
Phunguyen <yolosolo25@example.com>
2020-11-09 16:41:11 +0700

    Initial commit

---
+++ src/main/java/com/xtel/training/socket/client/ClientTCP.java
@@ -0,0 +1,33 @@
+package com.xtel.training.socket.client;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+public class ClientTCP {
+    public static void main(String[] args) throws IOException {
+        String sentence_to_server;
+        String sentence_from_server;
+
+        //tao inputStream(tu ban phim)
+        System.out.print("Input from client: ");
+        BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+        //lay chuoi ky tu nhap tu ban phim
+        sentence_to_server = inFromUser.readLine();
+        //tao socket cho client ket noi den server qua ID address và port number
+        Socket clientSocket = new Socket("192.168.1.191", 6543);
+        //tao outputStream noi voi Socket
+        DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+        //tao inputStream noi voi Socket
+        BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        //gui chuoi ky tu toi Server thong qua outputStream da noi voi Socket o tren
+        outToServer.writeBytes(sentence_to_server + '\n');
+        //doc tin tu Server thong qua InputSteam da noi voi socket
+        sentence_from_server = inFromServer.readLine();
+        System.out.println("FROM SERVER: " + sentence_from_server);
+        //dong lien ket socket
+        clientSocket.close();
+    }
+}
---
+++ src/main/java/com/xtel/training/socket/server/ServerTCP.java
@@ -0,0 +1,36 @@
+package com.xtel.training.socket.server;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServerTCP {
+    public static void main(String[] args) throws IOException {
+        String sentence_from_client;
+        String sentence_to_client;
+
+        //tao socket server, cho tai cong 6543
+        ServerSocket welcomeSocket = new ServerSocket(6543);
+        while(true) {
+            //cho yeu cau tu client
+            Socket connectionSocket = welcomeSocket.accept();
+
+            //tao input stream, noi toi socket
+            BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+
+            //tao output stream, noi toi socket
+            DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+
+            //doc thong tin tu socket
+            sentence_from_client = inFromClient.readLine();
+
+            sentence_to_client = sentence_from_client +" (Server accepted!)" + '\n';
+            //ghi du lieu ra socket
+            outToClient.writeBytes(sentence_to_client);
+            return;
+        }
+    }
+}
